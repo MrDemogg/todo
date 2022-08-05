cd9fc0fca68dbc0e5dae6a66547c68ea3298b608
demog <dg08gg@mail.ru>
2022-08-05 13:28:43 +0300

    взятия информации из localstorage для удобства

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import React, { Component } from "react";
 import './App.css';
 import trash from './trash-fill.svg';
-
-function App() {
+class App extends Component {
+  state = {
+    tasks: []
+  }
+  constructor() {
+    for (let i = 0; i < localStorage.length; i++) {
+      let nowKeyIs = localStorage.key(i);
+      if (nowKeyIs.startsWith("todoTask")) {
+        const task = {text: "", id: ""}
+        const data = localStorage.getItem(`todoTaks${i}`)
+        task.text = data.text;
+        task.id = data.id;
+        this.state.tasks.append(task)
+      }
+    }
+  }
   
 }
 
